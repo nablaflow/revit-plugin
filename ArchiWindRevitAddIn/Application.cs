4a7de6a282e09bd567cc537c32f4dc16faaec2c9
Federico Ravasio <ravasio.federico@gmail.com>
2026-01-09 17:51:23 +0100

    fix(ui): add tooltips to ribbon buttons

--- ArchiWindRevitAddIn/Application.cs
+++ ArchiWindRevitAddIn/Application.cs
@@ -28,23 +28,27 @@ namespace ArchiWindRevitAddIn
 
             viewsPanel.AddPushButton<Commands.PreviewGeometries>("Create")
                 .SetImage("/ArchiWindRevitAddIn;component/Resources/Icons/create-views-light@16.png")
-                .SetLargeImage("/ArchiWindRevitAddIn;component/Resources/Icons/create-views-light@32.png");
+                .SetLargeImage("/ArchiWindRevitAddIn;component/Resources/Icons/create-views-light@32.png")
+                .SetToolTip("Set up several new 3D views, each with geometries for the buildings, the surroundings, the terrain and the vegetation.\nCan be customised further.");
 
             viewsPanel.AddPushButton<Commands.ExportStls>("Export STLs")
                 .SetImage("/ArchiWindRevitAddIn;component/Resources/Icons/export-stls-light@16.png")
-                .SetLargeImage("/ArchiWindRevitAddIn;component/Resources/Icons/export-stls-light@32.png");
+                .SetLargeImage("/ArchiWindRevitAddIn;component/Resources/Icons/export-stls-light@32.png")
+                .SetToolTip("Export ArchiWind's 3D views into separate .STL files.\nCan be used to manually create a simulation on https://archiwind.nablaflow.io");
 
             var commandsPanel = Application.CreatePanel("Simulation", TAB_NAME);
 
             commandsPanel.AddPushButton<Commands.CreateSimulation>("Create")
                 .SetImage("/ArchiWindRevitAddIn;component/Resources/Icons/create-simulation-light@16.png")
-                .SetLargeImage("/ArchiWindRevitAddIn;component/Resources/Icons/create-simulation-light@32.png");
+                .SetLargeImage("/ArchiWindRevitAddIn;component/Resources/Icons/create-simulation-light@32.png")
+                .SetToolTip("Create a new ArchiWind simulation.\nUses ArchiWind's 3D views.");
 
             var settingsPanel = Application.CreatePanel("Settings", TAB_NAME);
 
             settingsPanel.AddPushButton<Commands.AccountSettings>("Account")
                 .SetImage("/ArchiWindRevitAddIn;component/Resources/Icons/user-settings-light@16.png")
-                .SetLargeImage("/ArchiWindRevitAddIn;component/Resources/Icons/user-settings-light@32.png");
+                .SetLargeImage("/ArchiWindRevitAddIn;component/Resources/Icons/user-settings-light@32.png")
+                .SetToolTip("View the current ArchiWind account and edit the API token.");
         }
 
         private static void InitialiseServices()
